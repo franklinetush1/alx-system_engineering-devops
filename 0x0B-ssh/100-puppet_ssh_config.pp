2937ba20a0d0493405eb0b41de18fee9ae2d11d4
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-25 22:10:06 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,7 @@
 # Changes SSH config file using puppet
 exec { 'ssh_config_update':
-  path        => ['/usr/bin', '/bin'],
-  command     => 'echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
-  refreshonly => true,
-  notify      => Service['ssh'],
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
 }
 
