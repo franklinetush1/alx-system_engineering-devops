40c3bcae61ab8aaf08dddd85f38173d018fbe43b
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-03 23:26:43 +0300

    Update 2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+class nginx {
+  exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+  }
+
+  exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+  }
+
+  file { '/etc/nginx/conf.d/custom_response_header.conf':
+    ensure  => present,
+    content => "add_header X-Served-By $hostname;",
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/conf.d/custom_response_header.conf'],
+  }
+}
+
+include nginx_custom_header
