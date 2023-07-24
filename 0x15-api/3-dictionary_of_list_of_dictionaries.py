680893221916aa7c3b6bcf000b2f09bc8128097f
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-24 20:29:47 +0300

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,33 +3,37 @@
 import json
 import requests
 
+def fetch_user_data(user_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    response = requests.get(url)
+    user_data = response.json()
+    return user_data['username']
 
-def export_tasks_to_json(url):
-    def fetch_user_data(user_id):
-        response = requests.get(url + f"users/{user_id}")
-        return response.json()['name']
-
-    response = requests.get(url + "todos")
-    task_list = response.json()
+def export_tasks_to_json():
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users_response = requests.get(base_url + "users")
+    users = users_response.json()
 
     data = {}
-    for task in task_list:
-        user_id = task['userId']
+    for user in users:
+        user_id = user.get("id")
         username = fetch_user_data(user_id)
-        task_data = {
-            "username": username,
-            "task": task["title"],
-            "completed": task["completed"]
-        }
-        if user_id in data:
-            data[user_id].append(task_data)
-        else:
-            data[user_id] = [task_data]
+
+        tasks_response = requests.get(base_url + "todos", params={"userId": user_id})
+        tasks = tasks_response.json()
+
+        task_data = []
+        for task in tasks:
+            task_data.append({
+                "username": username,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            })
+
+        data[user_id] = task_data
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump(data, jsonfile)
 
-
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    export_tasks_to_json(url)
+    export_tasks_to_json()
