ceefa9d7f491fc6bc9ce2be317d3b0ad36bea090
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-24 19:16:06 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -24,9 +24,9 @@ def export_tasks_to_json(tasks, user_id, username):
 if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/"
-    user_id = int(sys.argv[1])
+    user_id = sys.argv[1]
     user = requests.get(url + 'users/{}'.format(user_id)).json()
     task_list = requests.get(url + 'todos').json()
 
-    user_tasks = [task for task in task_list if task.get('userId') == user_id]
+    user_tasks = [task for task in task_list if task.get('userId') == int(user_id)]
     export_tasks_to_json(user_tasks, user_id, user['name'])
