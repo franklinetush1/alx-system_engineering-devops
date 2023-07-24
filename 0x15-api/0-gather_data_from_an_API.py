1e0525bc140d5de35afc32b555f4826d33b41f65
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-24 18:37:23 +0300

    Get data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
             totaltask += 1
             if task_list[i].get('completed') is True:
                 completed.append(task_list[i].get('title'))
-    print("Number of tasks completed = {}/{}"
-          .format(len(completed), totaltask))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'),len(completed), totaltask))
     [print("\t {}".format(task)) for task in completed]
