7141a1f01e31d9cbcbaaa6f37870864c846ae844
franklinetush1 <mburugufrankline1@gmail.com>
2023-08-08 17:50:05 +0000

    Api advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
+import requests
+
+def top_ten(subreddit):
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by franklinetush1@gmail.com)"}
+    parameters ={"limit": 10}
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    response = requests.get(url, headers=headers, params=parameters, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data")
+        [print(item.get("data").get("title")) for item in data.get("children")]
+    else:
+        print("None")
+        return
