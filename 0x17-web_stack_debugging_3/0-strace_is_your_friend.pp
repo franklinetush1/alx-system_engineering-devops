c7e5f7abd194eb928c4a2eee111a92b4f1acd500
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-09 17:31:24 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,8 @@
- # Ensure proper wording
-exec { 'correction':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+# Ensure proper wording
+package { 'apache2':
+  ensure => 'installed',
+}
+
+package { 'php':
+  ensure => 'installed',
 }
