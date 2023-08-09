82aaa420a821078444fa81a777a2210d87aa6ff5
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-09 17:33:47 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,3 +6,10 @@ package { 'apache2':
 package { 'php':
   ensure => 'installed',
 }
+
+file { '/etc/php/5.6/apache2/php.ini':
+  ensure => 'file',
+  content => template('your_module/php.ini.erb'),
+  require => Package['php'],
+  notify => Service['apache2'],
+}
