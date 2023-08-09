3b24b6e65d915d9444b01c8bcfeb9573896a93ef
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-09 17:27:25 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,11 @@
  # Ensure that the php package is installed first
 file { '/var/www/html/wp-settings.php':
-  ensure  => 'present',
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
-  require => Package['php'],
+  ensure  => 'file',
+  source  => 'puppet:///path/to/your/wp-settings.php',
+  notify  => Exec['replace'],
+}
+
+exec { 'replace':
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  refreshonly => true,
 }
