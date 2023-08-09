bc4d9f2e14e906fc9e870d3d91fbf0a39b6cf7ef
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-09 17:38:35 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,8 +5,7 @@ package { 'apache2':
 package { 'php':
   ensure => 'installed',
 }
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-  require => Package['apache2', 'php'],
+exec { 'fixspell':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
